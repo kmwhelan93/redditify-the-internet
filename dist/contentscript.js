4814a236ef6c53ec01e0508aeb9e447c9f3f5a06
Kevin Whelan <km.whelan93@gmail.com>
2017-10-21 09:28:38 -0700

    minor changes

--- dist/contentscript.js
+++ dist/contentscript.js
@@ -1,2 +1,5 @@
 console.log("opened content script");
-alert("opened content script");
+button = document.createElement('button')
+button.appendChild(document.createTextNode("Click Me"))
+button.onclick(() => alert("CLICKED!"))
+document.body.appendChild(button)
